2f8498383911c9daa84c2d24741e1b5f4015e62e
Ng1515 <62250813+Ng1515@users.noreply.github.com>
2020-10-30 18:57:21 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Password required</title>
+	<link rel="stylesheet" type="text/css" href="css/login.css">
+	<link rel = "icon" href =  
+"http://pngimg.com/uploads/un/un_PNG5.png" 
+        type = "image/x-icon"> 
+</head>
+<body>
+	<p>
+		Welcome back Pablo Bartonio, please put in your password
+	</p>
+		<div class="blurred-box">
+   		<div class="user-login-box">
+    		<span class="user-icon"></span>
+    			<form action="https://www.un.org/en/sections/general/documents/" method="post">
+   				 <div class="user-name">Pablo Bartonio</div>
+   				 <input class="user-password" type="password" placeholder="Password" pattern="Lucky1311" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" name="mot_de_passe" required />
+    				<br /><br />
+    			<input class="boutton" type="submit" name="log" id="log" value="Log In Here">
+			</form>
+  		</div>
+ 	 	</div>
+ 	 	<footer>
+ 	 		<p class="copyright">©2020 UN logo</p>
+ 	 		<p>
+ 	 			<a href="https://www.who.int/home"><img src="css/images/who-logo.png" class="who" /></a>
+ 	 			|
+ 	 			<a href="https://www.wfp.org/"><img src="css/images/wfp-logo.png" class="wfp" /></a>
+ 	 			|
+ 	 			<a href="https://www.unicef.org/"><img src="css/images/Unicef-Logo.png" class="unicef" /></a>
+ 	 			|
+
+ 	 			|
+ 	 			|
+ 	 			|
+ 	 		</p>
+ 	 	</footer>
+</body>
+</html>
